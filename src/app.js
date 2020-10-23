69ca518c20006df4730d67869b561c6d4b076533
Dariss Harris <darissharris@gmail.com>
2020-10-23 09:27:56 -0700

    added bookmark store

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const winston = require('winston');
+const bookmarks = require('./bookmarkStore');
 
 const app = express();
 
@@ -13,49 +14,66 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 // set up winston
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'info.log' })
-    ]
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
 });
 
 if (NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
 }
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
   
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        logger.error(`Unauthorized request to path: ${req.path}`);
-        return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    // move to the next middleware
-    next()
-  })
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+  res.send('Hello, world!');
+});
+
+app.get('/bookmarks', (req, res) => {
+  res.json(bookmarks);
+});
+
+app.get('/bookmarks/:id', (req, res) => {
+  const { id } = req.params;
+  const bookmark = bookmarks.find(bookmark => bookmark.id == id);
+
+  if(!bookmark){
+    logger.error(`Bookmark with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Bookmark Not Found');
+  }
+  res.json(bookmark);
 });
 
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
 });
 
 module.exports = app;
---
+++ src/bookmarkStore.js
@@ -0,0 +1,14 @@
+const bookmarks = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+},
+{
+  id: 2,
+  header: 'List One',
+  cardIds: [1]
+}];
+
+
+
+module.exports = bookmarks; 
