2a030e77427bfe8bdb5860ba2cfa27a71e62c79d
Joseguera <joseguera@uclaextension.edu>
2020-10-23 08:51:11 -0700

    added libraries

--- src/app.js
+++ src/app.js
@@ -4,12 +4,39 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const winston = require('winston');
 
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
+// set up winston
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'info.log' })
+    ]
+});
+
+if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+  })
 
 app.use(morgan(morganOption));
 app.use(helmet());
