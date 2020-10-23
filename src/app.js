81e025d4b8ac8432d9074aed85d3ba0fe66c948b
Dariss Harris <darissharris@gmail.com>
2020-10-23 12:11:11 -0700

    routers added

--- src/app.js
+++ src/app.js
@@ -3,31 +3,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const bookmarkRouter = require('./Bookmarks/bookmarkRoute');
+const logger = require('./logger');
 const { NODE_ENV } = require('./config');
-const winston = require('winston');
-const bookmarks = require('./bookmarkStore');
-const { v4: uuid } = require('uuid');
-
 
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
-// set up winston
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
 
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -46,90 +31,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-app.get('/bookmarks', (req, res) => {
-  res.json(bookmarks);
-});
-
-app.get('/bookmarks/:id', (req, res) => {
-  const { id } = req.params;
-  const bookmark = bookmarks.find(bookmark => bookmark.id == id);
-
-  if(!bookmark){
-    logger.error(`Bookmark with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Bookmark Not Found');
-  }
-  res.json(bookmark);
-});
-
-app.post('/bookmarks', (req, res) => {
-    const { title, content } = req.body;
-
-    if (!title) {
-        logger.error(`Title is required`);
-        return res
-            .status(400)
-            .send('Invalid data');
-    }
-
-    if (!content) {
-        logger.error(`Content is required`);
-        return res
-            .status(400)
-            .send('Invalid data');
-    }
-
-    // get an id
-    const id = uuid();
-
-    const bookmark = {
-        id,
-        title,
-        content
-    };
-
-    bookmarks.push(bookmark);
-
-    logger.info(`Bookmark with id ${id} created`);
-    res
-        .status(201)
-        .location(`http://localhost:8000/bookmarks/${id}`)
-        .json(bookmark);
-
-});
-
-app.delete('/bookmarks/:id', (req, res) => {
-    const { id } = req.params;
-
-    const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id == id);
-
-    if (bookmarkIndex === -1) {
-        logger.error(`Bookmark with id ${id} not found.`);
-        return res
-            .status(404)
-            .send('Not Found');
-    }
-
-    // // remove card from lists
-    // // assume cardIds are not duplicated in the cardsIds array
-    // lists.forEach(list => {
-    //     const cardIds = list.cardIds.filter(cid => cid !== id);
-    //     list.cardIds = cardIds;
-    // });
-
-    bookmarks.splice(bookmarkIndex, 1);
-
-    logger.info(`Card with id ${id} deleted`);
-
-    res
-        .status(204)
-        .end();
-});
+app.use(bookmarkRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
