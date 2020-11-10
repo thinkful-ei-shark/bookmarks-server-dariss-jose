e85be87ea296b69424403f14057a7307e0feeeae
Dariss Harris <darissharris@gmail.com>
2020-11-10 09:02:43 -0800

    validate token in config.js updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+  API_TOKEN: process.env.API_TOKEN || '2d3594bb-0eaf-4121-ba0d-263a69ac4b58',
 };
