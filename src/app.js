dbb5b8dca9e705f7460c9c8f1a17833d19b596a6
Dariss Harris <darissharris@gmail.com>
2020-11-10 16:52:35 -0800

    updated router and app.js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(bookmarkRouter);
+app.use('/bookmarks', bookmarkRouter);
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
--- src/bookmark-router.js
+++ src/bookmark-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { v4: uuid } = require('uuid');
 const logger = require('./logger');
 const bookmarks = require('./bookmarkStore');
-const { router } = require('./app');
+
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
@@ -51,24 +51,26 @@ bookmarkRouter
 bookmarkRouter
   .route('/:bookmark_id')
   .get((req, res) => {
-    const { bookmarks_id } = req.params;
-    const bookmark = bookmarks.find(bookmark => bookmark.id === bookmarks_id);
+    const { bookmark_id } = req.params;
+    console.log(bookmark_id);
+    const bookmark = bookmarks.find(bookmark => bookmark.id == bookmark_id);
 
     if(!bookmark){
-      logger.error(`Bookmark with id ${bookmarks_id} not found.`);
+      logger.error(`Bookmark with id ${bookmark_id} not found.`);
       return res
         .status(404)
         .send('Bookmark Not Found');
     }
     res.json(bookmark);
   })
+
   .delete((req, res) => {
-    const { id } = req.params;
+    const { bookmark_id } = req.params;
 
-    const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
-
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == bookmark_id);
+    console.log(bookmarkIndex);
     if (bookmarkIndex === -1) {
-      logger.error(`Bookmark with id ${id} not found.`);
+      logger.error(`Bookmark with id ${bookmark_id} not found.`);
       return res
         .status(404)
         .send('Not Found');
@@ -76,7 +78,7 @@ bookmarkRouter
 
     bookmarks.splice(bookmarkIndex, 1);
 
-    logger.info(`Bookmark with id ${id} deleted`);
+    logger.info(`Bookmark with id ${bookmark_id} deleted`);
 
     res
       .status(204)
